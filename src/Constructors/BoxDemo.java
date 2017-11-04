3d17aec35c165eb2a7c1e386e2e8c6428bd523ea
Shreyansh Murarka <shreyanshmurarka97@gmail.com>
2017-11-04 14:52:33 +0530

    Added Contructors and static

---
+++ src/Constructors/BoxDemo.java
@@ -0,0 +1,9 @@
+class BoxDemo{
+	public static void main(String args[]){
+		Box box1=new Box();
+		Box box2=new Box();
+
+		System.out.println("Box 1 volume =" + box1.volume());
+		System.out.println("Box 2 volume =" + box2.volume());
+	}
+}
